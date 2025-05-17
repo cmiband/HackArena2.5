15ff1959575b5cb1824a7b096015c09642501a0a
bartekA05 <33941168+cmiband@users.noreply.github.com>
2025-05-17 14:03:51 +0200

    Update Bot.cs

--- Bot/Bot.cs
+++ Bot/Bot.cs
@@ -80,7 +80,8 @@ public class Bot : IBot
     {
         GAZ,
         ZONE,
-        DEF
+        DEF,
+        OFF
     }
 
     public bool AmInZone(GameState gameState)
@@ -92,7 +93,7 @@ public class Bot : IBot
         }
         foreach (var zone in gameState.Zones)
         {
-            if (((zone.X < pos.x) && pos.x < (zone.X + zone.Width)) && ((zone.Y < pos.y) && (pos.y < zone.Y + zone.Height)))
+            if (((zone.X <= pos.x) && pos.x < (zone.X + zone.Width)) && ((zone.Y <= pos.y) && (pos.y < zone.Y + zone.Height)))
             {
                 return true;
             }
@@ -104,8 +105,35 @@ public class Bot : IBot
 
     public BotResponse NextMove(GameState gameState)
     {
+        this.currentState = this.CalculateCurrentState(gameState);
+        BotResponse response = this.GetBotResponseBasedOnState(this.currentState, gameState);
 
-        
+        return response;
+    }
+
+    private BotResponse GetBotResponseBasedOnState(State playerState, GameState gameState)
+    {
+        switch (playerState)
+        {
+            case State.DEF:
+                Console.WriteLine("DEF");
+                return this.HandleDefense(gameState);
+
+            case State.GAZ:
+                Console.WriteLine("GAZ");
+                return this.HandleGaz(gameState);
+
+            case State.OFF:
+                Console.WriteLine("OFF");
+                return this.HandleOffense(gameState);
+
+            default:
+                return BotResponse.Pass();
+        }
+    }
+
+    public BotResponse HandleOffense(GameState gameState)
+    {
         PositionWrapper myLocation = this.GetPlayerPosition(gameState);
 
         if (myLocation != null)
@@ -155,229 +183,8 @@ public class Bot : IBot
                 return BotResponse.Rotate(null, Rotation.Right);
             }
         }
-        else
-        {
-            Console.WriteLine("Brak przeciwnikow");
-        }
-        
 
-        
-        
-        /*
-        Console.WriteLine($"GameStateId: {gameState.Id}");
-        Console.WriteLine($"PlayerId: {gameState.PlayerId}");
-        Console.WriteLine($"Tick: {gameState.Tick}");
-
-        Console.WriteLine("I SEE THESE TEAMS:");
-        for (int i = 0; i < gameState.Teams.Length; i++)
-        {
-            var team = gameState.Teams[i];
-            Console.WriteLine($"TEAM: {i}");
-            Console.WriteLine($"  Name: {team.Name}");
-            Console.WriteLine($"  Color: {team.Color}");
-
-            if(team is OwnTeam ot)
-            {
-                Console.WriteLine($"  Score: {ot.Score}");
-            }
-            
-            Console.WriteLine($"  Players:");
-            for (int j = 0; j < team.Players.Length; j++)
-            {
-                var player = team.Players[j];
-                if (player is OwnPlayer op)
-                {
-                    Console.WriteLine($"  OwnPlayer:");
-                    Console.WriteLine($"    Id: {op.Id}");
-                    Console.WriteLine($"    Ping: {op.Ping}");
-                    Console.WriteLine($"    TicksToRegen: {op.TicksToRegen}");
-                }
-                else if (player is EnemyPlayer ep)
-                {
-                    Console.WriteLine($"  OwnPlayer:");
-                    Console.WriteLine($"    Id: {ep.Id}");
-                    Console.WriteLine($"    Ping: {ep.Ping}");
-                }
-            }
-        }
-
-        Console.WriteLine("Map:");
-        for (int y = 0; y < gameState.Map.GetLength(0); y++)
-        {
-            for (int x = 0; x < gameState.Map.GetLength(1); x++)
-            {
-                Tile tile = gameState.Map[y, x];
-                char symbol = ' ';
-
-                if (tile.ZoneIndex != null)
-                {
-                    int zoneIndex = (int)gameState.Map[y, x].ZoneIndex!;
-                    symbol = (char)zoneIndex;
-                }
-
-                foreach (var entity in gameState.Map[y, x].Entities)
-                {
-                    if (entity is Tile.Wall wall)
-                    {
-                        if(wall.Type == WallType.Solid)
-                        {
-                            symbol = '#';
-                        }
-                        else
-                        {
-                            symbol = 'O';
-                        }
-                    }
-                    else if (entity is Tile.OwnLightTank ownLightTank)
-                    {
-                        symbol = ownLightTank.Direction switch
-                        {
-                            Direction.Down => 'V',
-                            Direction.Left => '<',
-                            Direction.Up => '^',
-                            Direction.Right => '>',
-                            _ => throw new NotSupportedException()
-                        };
-
-                        // There is also turret direction
-                        // ownTank.Turret.Direction
-                    }
-                    else if (entity is Tile.OwnHeavyTank ownHeavyTank)
-                    {
-                        symbol = ownHeavyTank.Direction switch
-                        {
-                            Direction.Down => 'V',
-                            Direction.Left => '<',
-                            Direction.Up => '^',
-                            Direction.Right => '>',
-                            _ => throw new NotSupportedException()
-                        };
-
-                        // There is also turret direction
-                        // ownTank.Turret.Direction
-                    }
-                    else if (entity is Tile.EnemyHeavyTank || entity is Tile.EnemyLightTank)
-                    {
-                        symbol = '@';
-                    }
-                    else if (entity is Tile.Bullet bullet)
-                    {
-                        if (bullet.Type == BulletType.Stun)
-                        {
-                            symbol = bullet.Direction switch
-                            {
-                                Direction.Down => '↓',
-                                Direction.Left => '←',
-                                Direction.Up => '↑',
-                                Direction.Right => '→',
-                                _ => throw new NotSupportedException()
-                            };
-                        }
-                        else if(bullet.Type == BulletType.Double)
-                        {
-                            symbol = bullet.Direction switch
-                            {
-                                Direction.Down => '⇊',
-                                Direction.Left => '⇇',
-                                Direction.Up => '⇈',
-                                Direction.Right => '⇉',
-                                _ => throw new NotSupportedException()
-                            };
-                        }
-                        else if(bullet.Type == BulletType.Stun)
-                        {
-                            symbol = bullet.Direction switch
-                            {
-                                Direction.Down => '|',
-                                Direction.Left => '-',
-                                Direction.Up => '|',
-                                Direction.Right => '-',
-                                _ => throw new NotSupportedException()
-                            };
-                        }
-                        else if (bullet.Type == BulletType.Healing)
-                        {
-                            symbol = bullet.Direction switch
-                            {
-                                Direction.Down => 'H',
-                                Direction.Left => 'H',
-                                Direction.Up => 'H',
-                                Direction.Right => 'H',
-                                _ => throw new NotSupportedException()
-                            };
-                        }
-                    }
-                    else if (entity is Tile.Laser laser)
-                    {
-                        symbol = laser.Orientation switch
-                        {
-                            LaserDirection.Horizontal => '-',
-                            LaserDirection.Vertical => '|',
-                            _ => throw new NotSupportedException()
-                        };
-                    }
-                    else if (entity is Tile.Mine mine)
-                    {
-                        symbol = 'X';
-                    }
-                }
-                Console.Write(symbol);
-                Console.Write(' ');
-            }
-            Console.Write("\n");
-        }
-
-        Console.WriteLine("ZONES:");
-        for (int i = 0; i < gameState.Zones.Length; i++)
-        {
-            var zone = gameState.Zones[i];
-            Console.WriteLine($"   X: {zone.X}");
-            Console.WriteLine($"   Y: {zone.Y}");
-            Console.WriteLine($"   Index: {zone.Index}");
-            Console.WriteLine($"   Width: {zone.Width}");
-            Console.WriteLine($"   Shares:");
-            for (int j = 0; j < zone.Shares.Count; j++)
-            {
-                var key = zone.Shares.Keys.ToList()[j];
-                var value = zone.Shares.Values.ToList()[j];
-                Console.WriteLine($"      Key: {key} Value: {value}");
-            }
-        }
-        */
-        this.currentState = this.CalculateCurrentState(gameState);
-        BotResponse response = this.GetBotResponseBasedOnState(this.currentState, gameState);
-
-        
-        //Bot that randomly choses one of all possible bot responses.
-        var rand = new Random();
-        var X = rand.Next(0,22);
-        var Y = rand.Next(0,22);
-        return rand.Next(0, 19) switch
-        {
-            0 => BotResponse.Rotate(Rotation.Left, null),
-            
-            _ => throw new NotSupportedException(),
-        };
-
-
-        return response;
-    }
-
-    private BotResponse GetBotResponseBasedOnState(State playerState, GameState gameState)
-    {
-        switch (playerState)
-        {
-            case State.DEF:
-                Console.WriteLine("DEF");
-                return this.HandleDefense(gameState);
-
-            case State.GAZ:
-                Console.WriteLine("GAZ");
-                return this.HandleGaz(gameState);
-
-            default:
-                return BotResponse.Pass();
-        }
+        return BotResponse.Pass();
     }
 
     private BotResponse HandleGaz(GameState gameState)
@@ -431,10 +238,20 @@ public class Bot : IBot
 
     private State CalculateCurrentState(GameState gameState)
     {
-        if (AmInZone(gameState))
+        List<EnemyWrapper> enemies = this.GetEnemyPositions(gameState);
+        bool isInZone = AmInZone(gameState);
+
+        if (isInZone)
         {
-            return State.DEF;
-        }
+            if(enemies.Count == 0)
+            {
+                return State.DEF;
+            } 
+            else
+            {
+                return State.OFF;
+            }
+        } 
 
         return State.GAZ;
     }
